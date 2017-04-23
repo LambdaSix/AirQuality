dd271a7f5f4153bce7b69b23b395022af3fe81bf
nanomage <nanomage@gmail.com>
2017-04-23 14:02:28 +1000

    Added unity serialisation support to AQReactions

--- AirQuality/AQGasResourceExchange.cs
+++ AirQuality/AQGasResourceExchange.cs
@@ -7,7 +7,8 @@ namespace AirQuality
 {
 	public class AQGasResourceExchange : PartModule
 	{
-		public Dictionary<string, AQReaction> Reactions;
+		//public Dictionary<string, AQReaction> Reactions;
+		public List<AQReaction> Reactions;
 		AQsettings InstanceAQSettings;
 		[KSPField(isPersistant = true, guiActive = true)]
 		public string Description;
@@ -16,24 +17,24 @@ namespace AirQuality
 		public void UpdateAll(Vessel vessel, AQAir Air, double LivingVolume, double ScaleFactor)
 		{
 			KeyValuePair<string, double> limitingreagent; 
-			foreach (string reactionname in Reactions.Keys)
+			foreach (AQReaction reaction in Reactions)
 			{
-				print("[AQ:GRE] Evaluating Reaction " + reactionname + " " + Reactions[reactionname].Name);
-				limitingreagent = Reactions[reactionname].CalculateLimit(ScaleFactor, LivingVolume, Air, vessel);
+				print("[AQ:GRE] Evaluating Reaction " + reaction.Name);
+				limitingreagent = reaction.CalculateLimit(ScaleFactor, LivingVolume, Air, vessel);
 				if (limitingreagent.Value - 1.0f < float.Epsilon)
 				{
-					print("[AQ:GRE] reaction " + Reactions[reactionname].Name + " is not limited");
+					print("[AQ:GRE] reaction " + reaction.Name + " is not limited");
 				}
 				else
 				{
-					print("[AQ:GRE] reaction " + Reactions[reactionname].Name + " is limited by reagent " + limitingreagent.Key + " to scale of " + limitingreagent.Value);
+					print("[AQ:GRE] reaction " + reaction.Name + " is limited by reagent " + limitingreagent.Key + " to scale of " + limitingreagent.Value);
 				}
-				print("[AQ:GRE] reaction " + Reactions[reactionname].Name + " updating resources");
-				Reactions[reactionname].UpdateResources(part, ScaleFactor); 
+				print("[AQ:GRE] reaction " + reaction.Name + " updating resources");
+				reaction.UpdateResources(part, ScaleFactor); 
 				print("[AQ:GRE] finished updating resources, limiting factor " + limitingreagent.Value);
-                print("[AQ:GRE] reaction " + Reactions[reactionname].Name + " updating AQGases");
-				Reactions[reactionname].UpdateAir(Air, LivingVolume,ScaleFactor);
-                print("[AQ:GRE] Finished simulating " + reactionname + " " + Reactions[reactionname].Name);
+				print("[AQ:GRE] reaction " + reaction.Name + " updating AQGases");
+				reaction.UpdateAir(Air, LivingVolume,ScaleFactor);
+				print("[AQ:GRE] Finished simulating " + reaction.Name);
 			}
 			return;
 		}
@@ -43,7 +44,7 @@ namespace AirQuality
 			if (Reactions == null)
 			{
 				print("[AQ:GRE] initializing Reactions");
-				Reactions = new Dictionary<string, AQReaction>();
+				Reactions = new List<AQReaction>();
 			}
 			if (InstanceAQSettings == null)
 			{
@@ -68,7 +69,7 @@ namespace AirQuality
 			if (node.HasNode("AQReactions"))
 			{
 				print("[AQ:GRE] Loading reactions for " + Description);
-				Reactions = new Dictionary<string, AQReaction>();
+				Reactions = new List<AQReaction>();
 				foreach (ConfigNode reactionnode in node.GetNode("AQReactions").GetNodes())
 				{
 					print("[AQ:GRE] discovered " + node.GetNode("AQReactions").GetNodes().Count() + " reactions");
@@ -81,10 +82,10 @@ namespace AirQuality
 						print("[AQ:GRE] Loading invariant properties from " + reactiondefinition);
 						reactionloader.LoadInvariant(reactiondefinition);
 					}
-					Reactions.Add(reactionloader.Name, reactionloader);
-					print("[AQ:GRE] Loaded " + Reactions[reactionloader.Name].Name + " with " +
-						   Reactions[reactionloader.Name].GasReagents.Count + " gas reagents and " +
-						  Reactions[reactionloader.Name].ResourceReagents.Count + " resource reagents");
+					Reactions.Add(reactionloader);
+					print("[AQ:GRE] Loaded " + Reactions.Last().Name + " with " +
+					      Reactions.Last().GasReagents.Count + " gas reagents and " +
+					      Reactions.Last().ResourceReagents.Count + " resource reagents");
 				}
 				print("[AQ:GRE] Finished loading " + Reactions.Count + " reactions");
 			}
@@ -113,16 +114,16 @@ namespace AirQuality
 				inode = node.AddNode("AQReactions");
 			}
 			print("[AQ:GRE] Saving " + Reactions.Count + " reactions");
-			foreach (string reactionname in Reactions.Keys)
+			foreach (AQReaction reaction in Reactions)
 			{
-				if (!inode.HasNode(reactionname))
+				if (!inode.HasNode(reaction.Name))
 				{
-					print("[AQ:GRE] Creating node for " + reactionname);
-					inode.AddNode(reactionname);
+					print("[AQ:GRE] Creating node for " + reaction.Name);
+					inode.AddNode(reaction.Name);
 				}
-				print("[AQ:GRE] Saving reaction " + Reactions[reactionname].Name + " in status " +
-					  Reactions[reactionname].Status);
-				Reactions[reactionname].Save(inode.GetNode(reactionname));
+				print("[AQ:GRE] Saving reaction " + reaction.Name + " in status " + reaction.Status);
+				reaction.Save(inode.GetNode(reaction.Name));
+				//Reactions[reactionname].Save(inode.GetNode(reactionname));
 			}
 			print("[AQ:GRE] Saved " + inode.GetNodes().Count() + "reactions");
 			base.OnSave(node);
@@ -138,29 +139,29 @@ namespace AirQuality
 				// Just started running
 				LastUpdate = Planetarium.GetUniversalTime();
 				print("[AQ:GRE] Starting simulation, listing reactions");
-				foreach (string reactionname in Reactions.Keys)
+				foreach (AQReaction reaction in Reactions)
 				{
-					print("[AQ:GRE] Reaction " + reactionname + "has name " + Reactions[reactionname].Name);
-					print("[AQ:GRE]           Status:" + Reactions[reactionname].Status);
-					print("[AQ:GRE]     AlwaysActive:" + Reactions[reactionname].AlwaysActive);
-					print("[AQ:GRE]             Type:" + Reactions[reactionname].Type);
-					print("[AQ:GRE] Listing AQGas reagents of " + reactionname);
-					if (Reactions[reactionname].GasReagents.Count == 0)
+					print("[AQ:GRE] Reaction " + reaction.Name + "has name " + reaction.Name);
+					print("[AQ:GRE]           Status:" + reaction.Status);
+					print("[AQ:GRE]     AlwaysActive:" + reaction.AlwaysActive);
+					print("[AQ:GRE]             Type:" + reaction.Type);
+					print("[AQ:GRE] Listing AQGas reagents of " + reaction.Name);
+					if (reaction.GasReagents.Count == 0)
 					{
-						print("[AQ:GRE] Reaction" + reactionname + " has no AQGasReagents defined");
+						print("[AQ:GRE] Reaction" + reaction.Name + " has no AQGasReagents defined");
 					}
-					foreach (AQGasReagent gasreagentiterator in Reactions[reactionname].GasReagents)
+					foreach (AQGasReagent gasreagentiterator in reaction.GasReagents)
 					{
 						print("[AQ:GRE] AQGasReagent " + gasreagentiterator.Name);
 						print("[AQ:GRE]               IsLimiting:" + gasreagentiterator.IsLimiting);
 						print("[AQ:GRE]               Production:" + gasreagentiterator.Production);
 					}
-					print("[AQ:GRE] Listing resource reagents of " + reactionname);
-					if (Reactions[reactionname].GasReagents.Count == 0)
+					print("[AQ:GRE] Listing resource reagents of " + reaction.Name);
+					if (reaction.GasReagents.Count == 0)
 					{
-						print("[AQ:GRE] Reaction" + reactionname + " has no resource reagents defined");
+						print("[AQ:GRE] Reaction" + reaction.Name + " has no resource reagents defined");
 					}
-					foreach (AQResourceReagent resourcereagentiterator in Reactions[reactionname].ResourceReagents)
+					foreach (AQResourceReagent resourcereagentiterator in reaction.ResourceReagents)
 					{
 						print("[AQ:GRE] AQResourceReagent " + resourcereagentiterator.Name);
 						print("[AQ:GRE]               IsLimiting:" + resourcereagentiterator.IsLimiting);
@@ -171,7 +172,6 @@ namespace AirQuality
 			}
 			if ((Planetarium.GetUniversalTime() - LastUpdate) > InstanceAQSettings.SimulationStep)
 			{
-				AQAir air;
 				double ScaleFactor = Math.Min(((Planetarium.GetUniversalTime() - LastUpdate) / InstanceAQSettings.SimulationStep), InstanceAQSettings.MaxScaleFactor);
 				LastUpdate+= ScaleFactor* InstanceAQSettings.SimulationStep;
 				print("[AQ:GRE] Updating Air of " + part.Modules.OfType<HabitableVolume>().Single().Air.Count + " gases");
